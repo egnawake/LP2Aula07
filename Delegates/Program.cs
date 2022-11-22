4742358e1ec233496c96313e0f840039a595126c
João Inácio <egnawake@gmail.com>
2022-11-22 10:30:09 +0000

    Create Delegates project

---
+++ Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static void PrintUpper(string s)
+        {
+            Console.WriteLine(s.ToUpper());
+        }
+
+        public static void PrintLower(string s)
+        {
+            Console.WriteLine(s.ToLower());
+        }
+    }
+}
